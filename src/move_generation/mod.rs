b3b9a97a8385482937e79e36292f62296ae803a8
Theo Barollet <theo.barollet@ensimag.fr>
2018-12-21 17:40:45 +0100

    preparing legal move generation with pinned pieces

--- src/move_generation/mod.rs
+++ src/move_generation/mod.rs
@@ -1,4 +1,5 @@
 pub mod init_magic;
+pub mod moves;
 
 use std::ptr;
 use std::fmt;
@@ -9,7 +10,7 @@ use board::{Board, HalfBoard, BitBoard, KING_CASTLING_RIGHTS_MASKS, QUEEN_CASTLI
 
 use utils::*;
 
-use enum_primitive::FromPrimitive;
+pub use self::moves::*;
 
 // Perft tests for move generation, see move_generation/perft_tests.rs
 #[cfg(test)]
@@ -25,7 +26,7 @@ mod magic_factors_tests;
 // TODO make it legal (and not pseudo legal anymore)
 
 
-// unsafe attack table for rooks, bishops and queens
+// Attack table for rooks, bishops and queens
 // this is a black magic fancy table with shared attacks
 // See: https://www.chessprogramming.org/Magic_Bitboards
 //
@@ -55,7 +56,6 @@ lazy_static! {
 fn init_sliding_attack_tables() -> [u64; SLIDING_ATTACK_TABLE_SIZE] {
     let mut attack_table = [0; SLIDING_ATTACK_TABLE_SIZE];
     for square in 0u8..64 {
-        //MagicEntry::fill_attack_table(&mut attack_table, square);
         fill_attack_table(&mut attack_table, square);
     }
     println!("Magic table loaded");
@@ -94,185 +94,22 @@ pub fn bishop_attack(square: Square, occupancy: BitBoard) -> BitBoard {
     sliding_attack(magic_entry, occupancy, bishop_offset)
 }
 
-// An ExtendedMove is a 64 bits word following more or the less the extended
-// representation in https://www.chessprogramming.org/Encoding_Moves
-// MSB -------------------------------------------------------- LSB
-// latter   | ep5 .. ep0 | hf6 .. hf0 | csl3 .. csl0
-// 63 .. 39 |  34 .. 28  |  27 .. 20  |  19  ..  16
-// Move structure
-// prom | capt | sp1 | sp0 | dst5 .. dst0 | st5 .. st0
-//  15  |  14  |  13 | 12  |  11  ..  6   |  5  ..  0
-// TODO change the Move structure to a short Move structure and an ExtendedMove with all the
-// decoration
-#[derive(Clone, Copy)]
-pub struct Move(u16);
-#[derive(Clone, Copy)]
-pub struct ExtendedMove(u64);
-
-const SPECIAL0_FLAG: u16 = 1 << 12;
-const SPECIAL1_FLAG: u16 = 1 << 13;
-const CAPTURE_FLAG: u16 = 1 << 14;
-const PROMOTION_FLAG: u16 = 1 << 15;
-
-const FLAGS_RANGE: u16 = SPECIAL0_FLAG | SPECIAL1_FLAG | CAPTURE_FLAG | PROMOTION_FLAG;
-
-const DOUBLE_PUSH_FLAG: u16 = SPECIAL0_FLAG;
-const EN_PASSANT_CAPTURE_FLAG: u16 = CAPTURE_FLAG | DOUBLE_PUSH_FLAG;
-
-const KING_CASTLE_FLAG: u16 = SPECIAL1_FLAG;
-const QUEEN_CASTLE_FLAG: u16 = SPECIAL0_FLAG | SPECIAL1_FLAG;
-
-// [black castle, white castle]
-pub const KING_CASTLE_MOVES: BlackWhiteAttribute<Move> = BlackWhiteAttribute::new(
-    Move::raw_new(D1_SQUARE, B1_SQUARE).set_flags(KING_CASTLE_FLAG),
-    Move::raw_new(E1_SQUARE, G1_SQUARE).set_flags(KING_CASTLE_FLAG));
-
-pub const QUEEN_CASTLE_MOVES: BlackWhiteAttribute<Move> = BlackWhiteAttribute::new(
-    Move::raw_new(D1_SQUARE, F1_SQUARE).set_flags(QUEEN_CASTLE_FLAG),
-    Move::raw_new(E1_SQUARE, C1_SQUARE).set_flags(QUEEN_CASTLE_FLAG));
-
-pub const NULL_MOVE: Move = Move::raw_new(Square::new(0), Square::new(0));
-
-pub const CASTLING_RIGHTS_BITS_OFFSET: u8 = 16;
-pub const HALFMOVE_CLOCK_BITS_OFFSET: u8 = 20;
-pub const EN_PASSANT_SQUARE_BITS_OFFSET: u8 = 28;
-
-pub const CASTLING_RIGHTS_BITS_SIZE: u8 = 4;
-pub const HALFMOVE_CLOCK_BITS_SIZE: u8 = 7;
-pub const EN_PASSANT_SQUARE_BITS_SIZE: u8 = 6;
-
-impl Move {
-    // Creates a simple move with no side effect
-    fn quiet_move(from: Square, to: Square) -> Self {
-        Move(u16::from(from.0) + (u16::from(to.0) << 6))
-    }
-
-    // Creates a move with all the specified flags
-    fn tactical_move(from: Square, to: Square, flags: u16) -> Self {
-        Self::quiet_move(from, to).set_flags(flags)
-    }
-
-    // This is supposed to be called only for constant compilation and not an runtime
-    #[allow(clippy::cast_lossless)]
-    const fn raw_new(from: Square, to: Square) -> Self {
-        Move(from.0 as u16 + ((to.0 as u16) << 6))
-    }
-
-    // Helper to set some flags to the move
-    const fn set_flags(self, flags: u16) -> Self {
-        Move(self.0 | flags)
-    }
-
-    // Returns wether the move has the given flags set
-    fn has_flags(self, flags: u16) -> bool {
-        self.0 & flags != 0
-    }
-
-    fn has_exact_flags(self, flags: u16) -> bool {
-        self.0 & FLAGS_RANGE == flags
-    }
-
-    fn set_promoted_piece(self, piece: Piece) -> Self {
-        Move(self.0 | (piece as u16 & 0b11) << 12)
-    }
-
-    // Public interface for the board
-    pub fn origin_square(self) -> Square {
-        Square::new((self.0 & 0x3f) as u8)
-    }
-
-    pub fn destination_square(self) -> Square {
-        Square::new((self.0 >> 6) as u8 & 0x3f)
-    }
-
-    pub fn get_promotion_piece(self) -> Option<Piece> {
-        if self.has_flags(PROMOTION_FLAG) {
-            // from_u32 returns an Option, panic!s if the piece code is invalid
-            Piece::from_u16(self.0 >> 12 & 0b11)
-        } else {
-            None
-        }
-    }
-
-    // if the move is a double pawn push, returns the associated en passant target square
-    pub fn get_en_passant_target_square(self) -> Option<Square> {
-        if self.has_exact_flags(DOUBLE_PUSH_FLAG) {
-            Some(self.destination_square())
-        } else {
-            None
-        }
-    }
-
-    // If this is a castling move, returns the from and to squares of the associated tower
-    pub fn get_castling_rook(self, side_to_move: Color) -> Option<(Square, Square)> {
-        if self.has_exact_flags(KING_CASTLE_FLAG) {
-            Some((KING_CASTLE_ROOK_ORIGIN_SQUARES[side_to_move], KING_CASTLE_ROOK_DEST_SQUARES[side_to_move]))
-        } else if self.has_exact_flags(QUEEN_CASTLE_FLAG) {
-            Some((QUEEN_CASTLE_ROOK_ORIGIN_SQUARES[side_to_move], QUEEN_CASTLE_ROOK_DEST_SQUARES[side_to_move]))
-        } else {
-            None
-        }
-    }
-
-    // If this is an en passant capture, returns the captured pawn square
-    pub fn get_en_passant_capture_square(self) -> Option<Square> {
-        if self.has_exact_flags(EN_PASSANT_CAPTURE_FLAG) {
-            Some(self.destination_square().behind())
-        } else {
-            None
-        }
-    }
-
-    // Not triggered by en passant capture
-    pub fn is_capture(self) -> bool {
-        self.has_flags(CAPTURE_FLAG) & !self.has_exact_flags(EN_PASSANT_CAPTURE_FLAG)
-    }
+// Returns the xray attack of the given square for pinned pieces
+// See: https://www.chessprogramming.org/X-ray_Attacks_(Bitboards)
+fn xray_attack(magic_entry: &MagicEntry, occupancy: BitBoard, offset_function: fn(u64, u64) -> usize) -> BitBoard {
+    let attack = sliding_attack(magic_entry, occupancy, offset_function);
+    let occupancy = occupancy & !attack;
+    sliding_attack(magic_entry, occupancy, offset_function)
 }
 
-impl ExtendedMove {
-    pub fn get_raw_move(self) -> Move {
-        #[allow(clippy::cast_lossless)]
-        Move(self.0 as u16)
-    }
-
-    // value has to have trailing zeros not to overwrite some other states
-    pub fn set_board_state(self, value: u8, state: u8) -> Self {
-        ExtendedMove(self.0 | u64::from(value) << state)
-    }
-
-    pub fn get_board_state(self, state: u8, size: u8) -> u8 {
-        ((self.0 >> state) & (u64::max_value() >> (64-size))) as u8
-    }
+fn rook_xray_attack(square: Square, occupancy: BitBoard) -> BitBoard {
+    let magic_entry = &ROOK_ATTACK_TABLE[usize::from(square.0)];
+    xray_attack(magic_entry, occupancy, rook_offset)
 }
 
-impl PartialEq for Move {
-    fn eq(&self, other: &Self) -> bool {
-        self.0 & 0xffff == other.0 & 0xffff
-    }
-}
-
-// TODO clean transposition (for en passant)
-impl Transpose for Move {
-    fn transpose(&self) -> Self {
-        let origin_square = self.origin_square().transpose();
-        let dest_square = self.destination_square().transpose();
-        let mut transposed_move = Move(self.0);
-        transposed_move.0 &= !0xfff; // Clearing the old squares
-        transposed_move.0 |= u16::from(origin_square.0) + (u16::from(dest_square.0) << 6);
-
-        transposed_move
-    }
-}
-
-impl Transpose for ExtendedMove {
-    fn transpose(&self) -> Self {
-        let mov = self.get_raw_move();
-        let mut transposed_move = ExtendedMove(self.0);
-        transposed_move.0 &= !0xffff;
-        transposed_move.0 |= u64::from(mov.transpose().0);
-
-        transposed_move
-    }
+fn bishop_xray_attack(square: Square, occupancy: BitBoard) -> BitBoard {
+    let magic_entry = &BISHOP_ATTACK_TABLE[usize::from(square.0)];
+    xray_attack(magic_entry, occupancy, bishop_offset)
 }
 
 // This struct holds a reference to a Board and generates the legal moves from this POV
@@ -289,36 +126,47 @@ impl Transpose for ExtendedMove {
 // computation time
 #[derive(Copy, Clone)]
 pub struct LegalMoveGenerator {
-    // Even if this is an HalfBoard, there will be another generator for the other HalfBoard
-    // so we hide the distinction between a board and an halfboard.
-    //board: &'board HalfBoard,
-    color: Color, // We keep the Color to know which castling masks to use
-
+    // We don't use a permanent reference to a HalfBoard, we will borrow the HalfBoard each time we
+    // need it
     move_stack: [Move; 128], // Allocated on the program stack with a bounded size
     // NOTE: The maximum size is 128 even if we can construct a position with 218 moves
     // maybe we have to change this to 218 in the future
 
     // Internals
+    // Pinned pieces are stored along the 4 directions diagonal, antidiagonal, vertical and
+    // horizontal
+    // We hold the squares that are pinned and no more than 2 pieces can be pinned on the same
+    // direction
+    pinned_pieces: [[Square; 2]; 4],
+    number_of_pinned_pieces: [usize; 4], // pinned stack indexes (basically a usize for 0 1 2)
     // next move on the stack
     last_move: usize,
     // iterator index
     next_iterator_move: usize,
     // copies of the caslting rights, previous en passant state and halfmove clock
     // NOTE: the en passant square is held in the HalfBoard, so we need to restore the state to use
-    // it, maybe at some point
+    // it
     en_passant: Option<Square>,
     castling_rights: u8,
     halfmove_clock: u8,
 }
+
+
+type Direction = usize;
+const VERTICAL: Direction = 0;
+const HORIZONTAL: Direction = 1;
+const DIAGONAL: Direction = 2;
+const ANTIDIAGONAL: Direction = 3;
 
 impl LegalMoveGenerator {
     // Initialize a new LegalMoveGenerator by computing pinned pieces
     // It takes a reference to the current board and the color of the player we want to move
     pub fn new(halfboard: &HalfBoard, color: Color, castling_rights: u8, halfmove_clock: u8) -> Self {
         let mut generator = Self {
-            color,
+            move_stack: [NULL_MOVE; 128], // Placeholder
 
-            move_stack: [NULL_MOVE; 128],
+            pinned_pieces: [[A1_SQUARE; 2]; 4], // Placeholder
+            number_of_pinned_pieces: [0; 4],
 
             last_move: 0,
             next_iterator_move: 0,
@@ -326,41 +174,36 @@ impl LegalMoveGenerator {
             castling_rights,
             halfmove_clock,
         };
-        // compute pinned pieces
-        //
+        // we compute pinned pieces and store the result for evaluation
+        generator.compute_pinned_pieces(halfboard);
         // fetch basic moves information
-        generator.fetch_possible_moves(halfboard);
+        generator.fetch_possible_moves(halfboard, color);
 
         generator
     }
 
-    // Pushs the given move in the move stack
-    fn push(&mut self, pushed_move: Move) {
-        self.move_stack[self.last_move] = pushed_move;
-        self.last_move += 1;
+    fn push_pinned_piece(&mut self, pinned_square: Square, direction: Direction) {
+        self.pinned_pieces[direction][self.number_of_pinned_pieces[direction]] = pinned_square;
+        self.number_of_pinned_pieces[direction] += 1;
     }
 
-    // Helper to push all the possible promotions
-    fn push_promotions(&mut self, promotion_move: Move) {
-        for promoted_piece in &AVAILABLE_PROMOTION {
-            self.push(promotion_move.set_promoted_piece(*promoted_piece));
+    fn compute_pinned_pieces(&mut self, board: &HalfBoard) {
+        let white_king_square = (board[Piece::KING] & board[Color::WHITE]).as_square();
+        // Pinned by a bishop move
+        for bishop_square in (board[Piece::BISHOP] | board[Piece::QUEEN]) & board[Color::BLACK] {
+            let xray_attack = bishop_xray_attack(bishop_square, board.occupied_squares());
+            if xray_attack.has_square(white_king_square) {
+                self.push_pinned_piece(// TODO get pinned square from xray func)
+            }
         }
-    }
-
-    // Decorates the next move to be fetched for iteration with irreversible states
-    fn decorate_move(&self, mov: Move) -> ExtendedMove {
-        ExtendedMove(u64::from(mov.0))
-            .set_board_state(self.castling_rights, CASTLING_RIGHTS_BITS_OFFSET)
-            // En passant square is given from the side to play pov
-            .set_board_state(self.en_passant.map_or(0, |square| square.0), EN_PASSANT_SQUARE_BITS_OFFSET)
-            .set_board_state(self.halfmove_clock, HALFMOVE_CLOCK_BITS_OFFSET)
+        // Pinned by a rook move
     }
 
     // All the move generation logic
     // For captures, the captured piece is set when the move is iterated upon
     // TODO fetch only captures first and then quiet moves to make it lazier
     // ------------------------------------------------
-    fn fetch_possible_moves(&mut self, board: &HalfBoard) {
+    fn fetch_possible_moves(&mut self, board: &HalfBoard, color: Color) {
         // Simple pawn push ------------------------
         let pawns = board[Piece::PAWN] & board[Color::WHITE];
         let pushed_pawns = (pawns << 8) & board.empty_squares();
@@ -446,14 +289,41 @@ impl LegalMoveGenerator {
         let attack = KING_ATTACK_TABLE[king_square.as_index()];
         self.push_captures_quiets(board, king_square, attack);
         // Castle
-        let color = self.color;
-        if self.can_king_castle(board) {
+        if self.can_king_castle(board, color) {
             self.push(KING_CASTLE_MOVES[color]);
         }
-        if self.can_queen_castle(board) {
+        if self.can_queen_castle(board, color) {
             self.push(QUEEN_CASTLE_MOVES[color])
         }
         // -----------------------------------------
+    }
+
+    // Pushs the given move in the move stack
+    fn push(&mut self, pushed_move: Move) {
+        self.move_stack[self.last_move] = pushed_move;
+        self.last_move += 1;
+    }
+
+    // Helper to push all the possible promotions
+    fn push_promotions(&mut self, promotion_move: Move) {
+        for promoted_piece in &AVAILABLE_PROMOTION {
+            self.push(promotion_move.set_promoted_piece(*promoted_piece));
+        }
+    }
+
+    // Checks if the given square is pinned along the given direction
+    fn is_pinned(&self, square: Square, direction: Direction) -> bool {
+        (0..self.number_of_pinned_pieces[direction])
+            .any(|i| self.pinned_pieces[direction][i] == square)
+    }
+
+    // Decorates the next move to be fetched for iteration with irreversible states
+    fn decorate_move(&self, mov: Move) -> ExtendedMove {
+        ExtendedMove::from_raw_move(mov)
+            .set_board_state(self.castling_rights, CASTLING_RIGHTS_BITS_OFFSET)
+            // En passant square is given from the side to play pov
+            .set_board_state(self.en_passant.map_or(0, |square| square.0), EN_PASSANT_SQUARE_BITS_OFFSET)
+            .set_board_state(self.halfmove_clock, HALFMOVE_CLOCK_BITS_OFFSET)
     }
 
     // Helper for pieces that can perform captures and quiet moves at the same time
@@ -474,18 +344,17 @@ impl LegalMoveGenerator {
         self.push_captures_quiets(board, origin_square, attack);
     }
 
-    // TODO add the is in check function
     // Castling
-    fn can_king_castle(&self, board: &HalfBoard) -> bool {
-        (self.castling_rights & KING_CASTLING_RIGHTS_MASKS[self.color] != 0) // right to castle kingside
-        && (KING_CASTLE_EMPTY[self.color] & board.occupied_squares() == 0) // none of the squares on the way are occupied
-        && (KING_CASTLE_CHECK[self.color].all(|square| !self.is_in_check(board, square))) // squares crossed by the king are in check
+    fn can_king_castle(&self, board: &HalfBoard, color: Color) -> bool {
+        (self.castling_rights & KING_CASTLING_RIGHTS_MASKS[color] != 0) // right to castle kingside
+        && (KING_CASTLE_EMPTY[color] & board.occupied_squares() == 0) // none of the squares on the way are occupied
+        && (KING_CASTLE_CHECK[color].all(|square| !self.is_in_check(board, square))) // squares crossed by the king are in check
     }
 
-    fn can_queen_castle(&self, board: &HalfBoard) -> bool {
-        (self.castling_rights & QUEEN_CASTLING_RIGHTS_MASKS[self.color] != 0) // right to castle queenside
-        && (QUEEN_CASTLE_EMPTY[self.color] & board.occupied_squares() == 0) // none of the squares on the way are occupied
-        && (QUEEN_CASTLE_CHECK[self.color].all(|square| !self.is_in_check(board, square))) // squares crossed by the king are in check
+    fn can_queen_castle(&self, board: &HalfBoard, color: Color) -> bool {
+        (self.castling_rights & QUEEN_CASTLING_RIGHTS_MASKS[color] != 0) // right to castle queenside
+        && (QUEEN_CASTLE_EMPTY[color] & board.occupied_squares() == 0) // none of the squares on the way are occupied
+        && (QUEEN_CASTLE_CHECK[color].all(|square| !self.is_in_check(board, square))) // squares crossed by the king are in check
     }
 
     // Uses a super piece (not to rely on the other side move generator)
@@ -549,7 +418,6 @@ impl Iterator for LegalMoveGenerator {
             let iter_move = self.move_stack[self.next_iterator_move];
             self.next_iterator_move += 1;
 
-            // TODO fetch the captured piece only when the move is played
             // Decorate the move and returns it
             Some(self.decorate_move(iter_move))
         } else {
---
+++ src/move_generation/moves.rs
@@ -0,0 +1,190 @@
+// Module holding move encoding
+
+use utils::*;
+
+use enum_primitive::FromPrimitive;
+
+// An ExtendedMove is a 64 bits word following more or the less the extended
+// representation in https://www.chessprogramming.org/Encoding_Moves
+// MSB -------------------------------------------------------- LSB
+// latter   | ep5 .. ep0 | hf6 .. hf0 | csl3 .. csl0
+// 63 .. 39 |  34 .. 28  |  27 .. 20  |  19  ..  16
+// Move structure
+// prom | capt | sp1 | sp0 | dst5 .. dst0 | st5 .. st0
+//  15  |  14  |  13 | 12  |  11  ..  6   |  5  ..  0
+// TODO change the Move structure to a short Move structure and an ExtendedMove with all the
+// decoration
+#[derive(Clone, Copy)]
+pub struct Move(u16);
+#[derive(Clone, Copy)]
+pub struct ExtendedMove(u64);
+
+pub const SPECIAL0_FLAG: u16 = 1 << 12;
+pub const SPECIAL1_FLAG: u16 = 1 << 13;
+pub const CAPTURE_FLAG: u16 = 1 << 14;
+pub const PROMOTION_FLAG: u16 = 1 << 15;
+
+const FLAGS_RANGE: u16 = SPECIAL0_FLAG | SPECIAL1_FLAG | CAPTURE_FLAG | PROMOTION_FLAG;
+
+pub const DOUBLE_PUSH_FLAG: u16 = SPECIAL0_FLAG;
+pub const EN_PASSANT_CAPTURE_FLAG: u16 = CAPTURE_FLAG | DOUBLE_PUSH_FLAG;
+
+const KING_CASTLE_FLAG: u16 = SPECIAL1_FLAG;
+const QUEEN_CASTLE_FLAG: u16 = SPECIAL0_FLAG | SPECIAL1_FLAG;
+
+// [black castle, white castle]
+pub const KING_CASTLE_MOVES: BlackWhiteAttribute<Move> = BlackWhiteAttribute::new(
+    Move::raw_new(D1_SQUARE, B1_SQUARE).set_flags(KING_CASTLE_FLAG),
+    Move::raw_new(E1_SQUARE, G1_SQUARE).set_flags(KING_CASTLE_FLAG));
+
+pub const QUEEN_CASTLE_MOVES: BlackWhiteAttribute<Move> = BlackWhiteAttribute::new(
+    Move::raw_new(D1_SQUARE, F1_SQUARE).set_flags(QUEEN_CASTLE_FLAG),
+    Move::raw_new(E1_SQUARE, C1_SQUARE).set_flags(QUEEN_CASTLE_FLAG));
+
+pub const NULL_MOVE: Move = Move::raw_new(Square::new(0), Square::new(0));
+
+pub const CASTLING_RIGHTS_BITS_OFFSET: u8 = 16;
+pub const HALFMOVE_CLOCK_BITS_OFFSET: u8 = 20;
+pub const EN_PASSANT_SQUARE_BITS_OFFSET: u8 = 28;
+
+pub const CASTLING_RIGHTS_BITS_SIZE: u8 = 4;
+pub const HALFMOVE_CLOCK_BITS_SIZE: u8 = 7;
+pub const EN_PASSANT_SQUARE_BITS_SIZE: u8 = 6;
+
+impl Move {
+    // Creates a simple move with no side effect
+    pub fn quiet_move(from: Square, to: Square) -> Self {
+        Move(u16::from(from.0) + (u16::from(to.0) << 6))
+    }
+
+    // Creates a move with all the specified flags
+    pub fn tactical_move(from: Square, to: Square, flags: u16) -> Self {
+        Self::quiet_move(from, to).set_flags(flags)
+    }
+
+    // This is supposed to be called only for constant compilation and not an runtime
+    #[allow(clippy::cast_lossless)]
+    const fn raw_new(from: Square, to: Square) -> Self {
+        Move(from.0 as u16 + ((to.0 as u16) << 6))
+    }
+
+    // Helper to set some flags to the move
+    pub const fn set_flags(self, flags: u16) -> Self {
+        Move(self.0 | flags)
+    }
+
+    // Returns wether the move has the given flags set
+    pub fn has_flags(self, flags: u16) -> bool {
+        self.0 & flags != 0
+    }
+
+    fn has_exact_flags(self, flags: u16) -> bool {
+        self.0 & FLAGS_RANGE == flags
+    }
+
+    pub fn set_promoted_piece(self, piece: Piece) -> Self {
+        Move(self.0 | (piece as u16 & 0b11) << 12)
+    }
+
+    // Public interface for the board
+    pub fn origin_square(self) -> Square {
+        Square::new((self.0 & 0x3f) as u8)
+    }
+
+    pub fn destination_square(self) -> Square {
+        Square::new((self.0 >> 6) as u8 & 0x3f)
+    }
+
+    pub fn get_promotion_piece(self) -> Option<Piece> {
+        if self.has_flags(PROMOTION_FLAG) {
+            // from_u32 returns an Option, panic!s if the piece code is invalid
+            Piece::from_u16(self.0 >> 12 & 0b11)
+        } else {
+            None
+        }
+    }
+
+    // if the move is a double pawn push, returns the associated en passant target square
+    pub fn get_en_passant_target_square(self) -> Option<Square> {
+        if self.has_exact_flags(DOUBLE_PUSH_FLAG) {
+            Some(self.destination_square())
+        } else {
+            None
+        }
+    }
+
+    // If this is a castling move, returns the from and to squares of the associated tower
+    pub fn get_castling_rook(self, side_to_move: Color) -> Option<(Square, Square)> {
+        if self.has_exact_flags(KING_CASTLE_FLAG) {
+            Some((KING_CASTLE_ROOK_ORIGIN_SQUARES[side_to_move], KING_CASTLE_ROOK_DEST_SQUARES[side_to_move]))
+        } else if self.has_exact_flags(QUEEN_CASTLE_FLAG) {
+            Some((QUEEN_CASTLE_ROOK_ORIGIN_SQUARES[side_to_move], QUEEN_CASTLE_ROOK_DEST_SQUARES[side_to_move]))
+        } else {
+            None
+        }
+    }
+
+    // If this is an en passant capture, returns the captured pawn square
+    pub fn get_en_passant_capture_square(self) -> Option<Square> {
+        if self.has_exact_flags(EN_PASSANT_CAPTURE_FLAG) {
+            Some(self.destination_square().behind())
+        } else {
+            None
+        }
+    }
+
+    // Not triggered by en passant capture
+    pub fn is_capture(self) -> bool {
+        self.has_flags(CAPTURE_FLAG) & !self.has_exact_flags(EN_PASSANT_CAPTURE_FLAG)
+    }
+}
+
+impl ExtendedMove {
+    pub fn from_raw_move(raw_move: Move) -> ExtendedMove {
+        ExtendedMove(u64::from(raw_move.0))
+    }
+
+    pub fn get_raw_move(self) -> Move {
+        #[allow(clippy::cast_lossless)]
+        Move(self.0 as u16)
+    }
+
+    // value has to have trailing zeros not to overwrite some other states
+    pub fn set_board_state(self, value: u8, state: u8) -> Self {
+        ExtendedMove(self.0 | u64::from(value) << state)
+    }
+
+    pub fn get_board_state(self, state: u8, size: u8) -> u8 {
+        ((self.0 >> state) & (u64::max_value() >> (64-size))) as u8
+    }
+}
+
+impl PartialEq for Move {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 & 0xffff == other.0 & 0xffff
+    }
+}
+
+// TODO clean transposition (for en passant)
+impl Transpose for Move {
+    fn transpose(&self) -> Self {
+        let origin_square = self.origin_square().transpose();
+        let dest_square = self.destination_square().transpose();
+        let mut transposed_move = Move(self.0);
+        transposed_move.0 &= !0xfff; // Clearing the old squares
+        transposed_move.0 |= u16::from(origin_square.0) + (u16::from(dest_square.0) << 6);
+
+        transposed_move
+    }
+}
+
+impl Transpose for ExtendedMove {
+    fn transpose(&self) -> Self {
+        let mov = self.get_raw_move();
+        let mut transposed_move = ExtendedMove(self.0);
+        transposed_move.0 &= !0xffff;
+        transposed_move.0 |= u64::from(mov.transpose().0);
+
+        transposed_move
+    }
+}
